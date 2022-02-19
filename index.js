3e1ad22b48bf0b3cc7d517b1237098ad703fe1ac
Tạ Quang Toàn <toanta0326@gmail.com>
2022-02-19 09:58:28 +0700

    add getPost and getPostsBySearch

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from 'dotenv'
+import postRoutes from "./routes/posts.js"
+import userRoutes from "./routes/user.js"
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
+
+app.get('/', (req, res) => {
+    res.send('Connect MERN_API_APP Successful!')
+})
+
+// test
+
+const URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((err) => console.log(err));
